f01f73a3afba069b32d14bd7b1aed2c23824b72f
rik6789 <136086686+rik6789@users.noreply.github.com>
2025-08-21 10:32:52 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
-module.exports = {
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   async rewrites() {
     return [
-      { source: '/search/word/:word.html', destination: '/search/word/:word' }
+      {
+        source: '/search/word/:word',
+        destination: '/?word=:word',
+      },
+      {
+        source: '/search/word/:word.html',
+        destination: '/?word=:word',
+      },
     ];
-  }
-};
+  },
+}
+
+module.exports = nextConfig
