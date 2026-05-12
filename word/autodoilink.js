4a85a10ff230c9349fc36bbf42f025fb5fd95354
rik6789 <136086686+rik6789@users.noreply.github.com>
2026-05-12 12:30:32 +0700

    Update autodoilink.js

--- word/autodoilink.js
+++ word/autodoilink.js
@@ -1,6 +1,10 @@
-const SITE_URL = "https://www.luyentiengtrung.c1om";
-const HANZI_BASE = "/hanzi1/";
+const SITE_URL = "https://www.luyentiengtrung.com";
 
+const HANZI_BASE = "/hanzi/";
+
+/*
+  Tạo link học ngay
+*/
 document.querySelectorAll(".learn-btn").forEach(btn => {
 
     const word = btn.dataset.word;
@@ -10,3 +14,22 @@ document.querySelectorAll(".learn-btn").forEach(btn => {
         encodeURIComponent(word) +
         ".html";
 });
+
+
+/*
+  Tạo canonical tự động
+*/
+const currentWord = document.body.dataset.word;
+
+const canonical =
+    SITE_URL +
+    "/word/" +
+    encodeURIComponent(currentWord) +
+    ".html";
+
+const canonicalTag =
+    document.getElementById("canonical");
+
+if (canonicalTag) {
+    canonicalTag.href = canonical;
+}
